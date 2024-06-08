304d28ec3ae6110e386b17826e0605ebd589f22f
Abdiaziz82 <abdiazizhared64@gmail.com>
2024-06-08 15:32:01 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,52 @@
 const farmAnimals = 'cow horse sheep pig chicken';
 
+
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+
+console.log(moo);   // Outputs: cow
+console.log(neigh); // Outputs: horse
+console.log(baa);   // Outputs: sheep
+console.log(oink);  // Outputs: pig
+console.log(cluck); // Outputs: chicken
+
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
+
+console.log(bessie); // Outputs: cow
+console.log(dolly);  // Outputs: sheep
+console.log(babe);   // Outputs: pig
+console.log(little); // Outputs: chicken
+const [blackAndWhite, , black, pink] = farmAnimals.split(' ');
+
+console.log(blackAndWhite); // Outputs: cow
+console.log(black);         // Outputs: sheep
+console.log(pink);          // Outputs: pig
+
+
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+
+console.log(red);    // Outputs: red
+console.log(orange); // Outputs: orange
+console.log(yellow); // Outputs: yellow
+console.log(green);  // Outputs: green
+console.log(blue);   // Outputs: blue
+console.log(indigo); // Outputs: indigo
+console.log(violet); // Outputs: violet
+const [r, o, y, g, b, , v] = colors;
+
+console.log(r); // Outputs: red
+console.log(o); // Outputs: orange
+console.log(y); // Outputs: yellow
+console.log(g); // Outputs: green
+console.log(b); // Outputs: blue
+console.log(v); // Outputs: violet
+
+const [, , , , , indg] = colors;
+
+console.log(indg); // Outputs: indigo
+
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -9,6 +55,14 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+const { muppetName, color, song, job, partner } = muppet;
+
+console.log(muppetName); // Outputs: Miss Piggy
+console.log(color);      // Outputs: pink
+console.log(song);       // Outputs: Never Before, Never Again
+console.log(job);        // Outputs: Cast member of The Muppet Show
+console.log(partner);    // Outputs: Kermit
+
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,6 +78,19 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+
+const { 
+  album: { theMuppetMovie: { song2, song4 } },
+  nestedJob,
+  nestedPartner
+} = nestedMuppet;
+
+console.log(song2);         // Outputs: Moving Right Along
+console.log(song4);         // Outputs: I Hope That Something Better Comes Along
+console.log(nestedJob);     // Outputs: Host of The Muppet Show
+console.log(nestedPartner); // Outputs: Miss Piggy
+
+
 
 // Strings
 
